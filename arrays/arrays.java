e20ee9dc76548efdf36148fd15f494c8ece0c3c4
anushna76 <anushna7639@gmail.com>
2025-05-09 00:43:34 +0530

    Add files via upload

---
+++ arrays/arrays.java
@@ -0,0 +1,17 @@
+package com.tns.arrays;
+//reversing an array
+public class arrays {
+	public static void main(String[] args) {
+		int arr[]= {1,2,3};
+		
+		int n=arr.length;
+		System.out.println(n);
+		for(int j=n-1;j>=0;j--) {
+			
+			System.out.println(arr[j]);
+		}
+		
+		}
+	}
+
+
